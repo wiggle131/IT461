6d7f7d1cd7cffee8cb664da32fc74ead2b651ec0
jhosie-espantaleon <jhosie.espantaleon@symph.co>
2022-05-11 11:47:56 +0800

    Implements fields and metadata on /cats and /cats/<id>

--- server/src/app.py
+++ server/src/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, Blueprint
 from v1.dog.router import DogRouter
+from v1.cat.router import CatRouter
 
 app = Flask(__name__)
 
 bp_dogs = Blueprint('dogs', __name__, url_prefix='/v1/dogs')
+bp_cats = Blueprint('cats',__name__, url_prefix='/v1/cats')
 DogRouter.handler(bp_dogs)
+CatRouter.handler(bp_cats)
 app.register_blueprint(bp_dogs)
+app.register_blueprint(bp_cats)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=6000)
